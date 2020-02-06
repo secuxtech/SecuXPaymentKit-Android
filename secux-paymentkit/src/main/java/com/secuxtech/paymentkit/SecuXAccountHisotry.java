ec2cb6a1a95274a51d3af195b0f0cc2dd0ac6c7f
maochunsun <maochuns.sun@gmail.com>
2020-02-06 11:49:52 +0800

    Create SecuXAccountHisotry.java

---
+++ secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXAccountHisotry.java
@@ -0,0 +1,12 @@
+package com.secuxtech.paymentkit;
+
+public class SecuXAccountHisotry {
+    public String address = "";
+    public String tx_type = "";
+    public Double amount = Double.valueOf(0);
+    public String amount_symbol = "";
+    public Double formatted_amount = Double.valueOf(0);
+    public Double amount_usd = Double.valueOf(0);
+    public String timestamp = "";
+    public String detailslUrl = "";
+}
