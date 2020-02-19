73d22f10d63b852eba12711ca963b5d01aa9670e
maochunsun <maochuns.sun@gmail.com>
2020-02-19 18:05:13 +0800

    deleted

--- secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXAccountBalance.java
+++
@@ -1,8 +0,0 @@
-package com.secuxtech.paymentkit;
-
-public class SecuXAccountBalance {
-
-    public Double mBalance = Double.valueOf(0);
-    public Double mFormatedBalance = Double.valueOf(0);
-    public Double mUSDBalance = Double.valueOf(0);
-}
