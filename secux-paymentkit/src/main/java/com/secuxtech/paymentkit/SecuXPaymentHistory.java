24ef5295402b3a9aeac5b99b1627f0d97485140d
maochunsun <maochuns.sun@gmail.com>
2020-02-20 17:48:25 +0800

    Update SecuXPaymentHistory.java

--- secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXPaymentHistory.java
+++ secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXPaymentHistory.java
@@ -8,14 +8,13 @@ import org.json.JSONObject;
 
 public class SecuXPaymentHistory {
     public Integer  mID                 = -1;
-    public Integer  mParentID           = -1;
     public Integer  mStoreID            = -1;
-    public Integer  mDevID              = -1;
+    public String   mStoreName          = "";
+
     public String   mUserAccountName    = "";
-    public String   mTransactionID      = "";
     public String   mTransactionCode    = "";
     public String   mTransactionType    = "";
-    public String   mExchangeStoreID    = "";
+
     public String   mPayPlatform        = "";
     public String   mPayChannel         = "";
     public String   mCoinType           = "";
@@ -24,30 +23,16 @@ public class SecuXPaymentHistory {
     public String   mTransactionStatus  = "";
     public String   mTransactionTime    = "";
     public String   mRemark             = "";
-    public String   mSysRemark          = "";
-    //public String   mOperator           = "";
 
     SecuXPaymentHistory(JSONObject hisJson) throws Exception{
         try{
             mID = hisJson.getInt("id");
-
-            if (hisJson.getString("parentID").compareTo("null")!=0) {
-                mParentID = hisJson.getInt("parentID");
-            }
             mStoreID = hisJson.getInt("storeID");
-            mDevID = hisJson.getInt("deviceID");
+            mStoreName = hisJson.getString("storeName");
+
             mUserAccountName = hisJson.getString("account");
-
-            if (hisJson.getString("transactionId").compareTo("null")!=0) {
-                mTransactionID = hisJson.getString("transactionId");
-            }
-
             mTransactionCode = hisJson.getString("transactionCode");
             mTransactionType = hisJson.getString("transactionType");
-
-            if (hisJson.getString("exchangeStoreID").compareTo("null")!=0) {
-                mExchangeStoreID = hisJson.getString("exchangeStoreID");
-            }
 
             mPayPlatform = hisJson.getString("payPlatform");
             mPayChannel = hisJson.getString("payChannel");
@@ -57,10 +42,6 @@ public class SecuXPaymentHistory {
             mTransactionStatus = hisJson.getString("transactionStatus");
             mTransactionTime = hisJson.getString("transactionTime");
             mRemark = hisJson.getString("remark");
-
-            if (hisJson.getString("sysRemark").compareTo("null")!=0) {
-                mSysRemark = hisJson.getString("sysRemark");
-            }
 
         }catch (Exception e){
             throw e;
