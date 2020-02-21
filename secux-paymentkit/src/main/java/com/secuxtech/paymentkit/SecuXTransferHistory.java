54a7eaa57462a09ce96a1de3d00d5140d422c045
maochunsun <maochuns.sun@gmail.com>
2020-02-21 16:36:22 +0800

    Update SecuXTransferHistory.java

--- secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXTransferHistory.java
+++ secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXTransferHistory.java
@@ -12,30 +12,32 @@ import org.json.JSONObject;
  * Created by maochuns.sun@gmail.com on 2020-02-19
  */
 
+import java.math.BigDecimal;
+
 import static com.secuxtech.paymentkit.RestRequestHandler.TAG;
 
 public class SecuXTransferHistory {
-    public String mTxHash           = "";
-    public String mAddress          = "";
-    public String mTxType           = "";
-    public Double mAmount           = Double.valueOf(0);
-    public String mAmountSymbol     = "";
-    public String mFeeSymbol        = "";
-    public Double mFormattedAmount  = Double.valueOf(0);
-    public Double mAmountUsd        = Double.valueOf(0);
-    public String mTimestamp        = "";
-    public String mDetailslUrl      = "";
+    public String       mTxHash           = "";
+    public String       mAddress          = "";
+    public String       mTxType           = "";
+    public BigDecimal   mAmount           = null;
+    public String       mAmountSymbol     = "";
+    public String       mFeeSymbol        = "";
+    public BigDecimal   mFormattedAmount  = null;
+    public BigDecimal   mAmountUsd        = null;
+    public String       mTimestamp        = "";
+    public String       mDetailslUrl      = "";
 
     SecuXTransferHistory(JSONObject historyJson) throws Exception{
         try{
             mTxHash = historyJson.getString("txHash");
             mAddress = historyJson.getString("address");
             mTxType = historyJson.getString("tx_type");
-            mAmount = historyJson.getDouble("amount");
+            mAmount = new BigDecimal(historyJson.getString("amount"));
             mAmountSymbol = historyJson.getString("amount_symbol");
             mFeeSymbol = historyJson.getString("fee_symbol");
-            mFormattedAmount = historyJson.getDouble("formatted_amount");
-            mAmountUsd = historyJson.getDouble("amount_usd");
+            mFormattedAmount = new BigDecimal(historyJson.getString("formatted_amount"));
+            mAmountUsd = new BigDecimal(historyJson.getString("amount_usd"));
             mTimestamp = historyJson.getString("timestamp");
             mDetailslUrl = historyJson.getString("detailsUrl");
         }catch (Exception e){
