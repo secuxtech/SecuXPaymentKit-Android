f9fea9f486e2249edde289a81bec2367ddd2c57f
maochunsun <maochuns.sun@gmail.com>
2020-03-16 17:30:32 +0800

    Update SecuXPaymentHistory.java

--- secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXPaymentHistory.java
+++ secux-paymentkit/src/main/java/com/secuxtech/paymentkit/SecuXPaymentHistory.java
@@ -29,7 +29,7 @@ public class SecuXPaymentHistory {
     public String       mRemark             = "";
     public String       mDetailsUrl         = "";
 
-    SecuXPaymentHistory(){
+    public SecuXPaymentHistory(){
 
     }
 
